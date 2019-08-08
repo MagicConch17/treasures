f8e8a1b983a26564bcc93faa2bc9f378abbb1e08
xiaochou27 <higanbana98@hotmail.com>
2019-08-08 18:04:33 +0800

    pole
    增加说明文档

--- php/poletree/PoleTree.php
+++ php/poletree/PoleTree.php
@@ -1,13 +1,9 @@
 <?php
 /**
- * @Descripttion: 
- * @version: 
- * @Author: clown
- * @Date: 2019-08-07 18:05:57
- * @LastEditors: clown
- * @LastEditTime: 2019-08-07 18:06:43
- */
-
+ * @description: 根据数组生成树形结构
+ * @param {array} $array
+ * @return: array
+ */ 
 function generateTree($array)
 {
     //第一步 构造数据
@@ -25,4 +21,24 @@ function generateTree($array)
         }
     }
     return $tree;
+}
+
+/**
+ * @description: 根据对象生成树形结构
+ * @param {obj} $permissions 需要生成的对象
+ * @param {int} $id 父级
+ * @param {int} $count 层级
+ * @return: obj 排序后级别
+ */
+function getTree($permissions, $id = 0, $count = 0)
+{
+    static $res = array();
+    foreach ($permissions as $permission) {
+        if ($permission->parent_id == $id) {
+            $permission->count = $count;
+            $res[] = $permission;
+            $this->getTree($permissions, $permission->id, $count+1);
+        }
+    }
+    return $res;
 }
